af55715cc8f6a330c8599aa99c626cee158cf766
sunghee-cho <bluejeansh@hanmail.net>
2022-03-21 15:24:56 +0900

    Update GithubBootApplication.java

--- github_boot/src/main/java/edu/multi/boot1/GithubBootApplication.java
+++ github_boot/src/main/java/edu/multi/boot1/GithubBootApplication.java
@@ -9,6 +9,7 @@ public class GithubBootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GithubBootApplication.class, args);
 		System.out.println("깃헙push용도");
+		System.out.println("원격저장소 추가 라인");
 	}
 
 }
