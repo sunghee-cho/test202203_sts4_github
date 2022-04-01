03b426fa73db33d68fbb45c3bc3069bdc8b098ba
me <bluejeansh@hanmail.net>
2022-04-01 13:48:52 +0900

    주인 다시 add

--- github_boot/src/main/java/edu/multi/boot1/GithubBootApplication.java
+++ github_boot/src/main/java/edu/multi/boot1/GithubBootApplication.java
@@ -9,6 +9,10 @@ public class GithubBootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GithubBootApplication.class, args);
 		System.out.println("깃헙push용도");
+		System.out.println("원격저장소 추가 라인");
+		System.out.println("소스주인 추가 라인");
+		System.out.println("소스주인 또 추가 라인");	
+		System.out.println("소스주인 또또 추가 라인");		
 	}
 
 }
